ae3ea7674b4f0fa82836140520ce21a34b4a94ef
Vinoth Kumar <vinoct6@gmail.com>
2015-09-06 22:49:59 +0530

    Added POST function, but not working

--- src/main/java/com/services/rest/ActivityResource.java
+++ src/main/java/com/services/rest/ActivityResource.java
@@ -2,11 +2,14 @@ package com.services.rest;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedHashMap;
 
 import com.services.model.Activity;
 import com.services.model.User;
@@ -16,6 +19,16 @@ import com.services.repository.ActivityResourceStub;
 public class ActivityResource {
 
 		private ActivityResourceStub stub = new ActivityResourceStub();
+		
+		@POST
+		@Path("activity")
+		@Consumes(MediaType.APPLICATION_XML)
+		//@Produces(MediaType.TEXT_PLAIN)
+		public void createNewActivity(MultivaluedHashMap<String, String> params){
+			System.out.println(params);
+			//return "vinoth";
+		}
+		
 		
 		@GET//http://localhost:8080/resttutorial/v1/activities
 		@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
